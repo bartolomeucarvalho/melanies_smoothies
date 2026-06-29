748fca4c2dbab04d7dff31028d549e4f1857a34b
bartolomeucarvalho <bartolomeucarvalho19@gmail.com>
2026-06-29 11:04:26 +0300

    Update API call to fetch fruit information dynamically

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
         # make API call
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         # store obtained data in a df
         sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width= 'Stretch')
 
